490cf256b36a5db55febd7253644be79589625f8
mamota97 <41388713+mamota97@users.noreply.github.com>
2023-07-13 20:13:21 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,11 +1,10 @@
 """
 Neste problema, você deverá exibir uma lista de 1 a 100, um em cada linha, com as seguintes exceções:
-
     Números divisíveis por 3 deve aparecer como 'Fizz' ao invés do número;
     Números divisíveis por 5 devem aparecer como 'Buzz' ao invés do número;
     Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'.
-
 """
+
 list = []
 for item in range(1, 31):
     if item % 3 == 0:
