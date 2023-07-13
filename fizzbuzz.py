d958c821d89a6a50c7df6446f03f247e1095de35
mamota97 <41388713+mamota97@users.noreply.github.com>
2023-07-13 20:54:37 -0300

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,15 +5,18 @@ Neste problema, você deverá exibir uma lista de 1 a 100, um em cada linha, com
     Números divisíveis por 3 e 5 devem aparecer como 'FizzBuzz' ao invés do número'.
 """
 
+p = 0
 list = []
-for item in range(0, 100):
-    if item % 3 == 0:
-        list.append("fizz")
-        if item % 5 == 0:
-            list.append("fizzbuzz")
-    elif item % 5 == 0:
-        list.append("buzz")
-    else:
-        list.append(item)
-
-print(list)
+for numero in range(1, 101):
+    list.append(numero)
+    if list[p] % 3 == 0:
+        if list[p] % 3 == 0 and list[p] % 5 == 0:
+            list[p] = "fizzbuzz"
+        else:
+            list[p] = "fizz"
+    elif list[p] % 5 == 0:
+        if list[p] % 5 == 0 and list[p] % 3 == 0:
+            list[p] = "fizzbuzz"
+        else:
+            list[p] = "buzz"
+    p += 1
